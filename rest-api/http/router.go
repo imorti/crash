06774aec416731d027006685b2006ee45c6ca910
imorti <imorti@gmail.com>
2020-02-22 18:13:58 -0800

    updated comments

--- rest-api/http/router.go
+++ rest-api/http/router.go
@@ -2,6 +2,7 @@ package router
 
 import "net/http"
 
+// Router - interface
 type Router interface {
 	GET(uri string, f func(resp http.ResponseWriter, req *http.Request))
 	POST(uri string, f func(resp http.ResponseWriter, req *http.Request))
