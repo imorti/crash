2cf13c052c34b271821463342df99df1883de3fb
imorti <imorti@gmail.com>
2020-02-22 19:36:15 -0800

    added tests for service

--- rest-api/service/post-service_test.go
+++ rest-api/service/post-service_test.go
@@ -3,8 +3,49 @@ package service
 import (
 	"testing"
 
+	"github.com/imorti/crash/rest-api/entity"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (mock *MockRepository) Save(post *entity.Post) (*entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+
+}
+
+func (mock *MockRepository) FindAll() ([]entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entity.Post), args.Error(1)
+}
+
+func TestFindAll(t *testing.T) {
+	mockRepo := new(MockRepository)
+
+	var identifier int64 = 1
+
+	post := entity.Post{ID: identifier, Title: "A", Text: "B"}
+	//Set up expectation
+	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
+
+	testService := NewPostService(mockRepo)
+
+	result, _ := testService.FindAll()
+
+	// Mock assertion: Behavioral
+	mockRepo.AssertExpectations(t)
+
+	//Data Assertion
+	assert.Equal(t, identifier, result[0].ID)
+	assert.Equal(t, "A", result[0].Title)
+	assert.Equal(t, "B", result[0].Text)
+}
 
 func TestValidateEmptyPost(t *testing.T) {
 	testService := NewPostService(nil)
@@ -12,4 +53,15 @@ func TestValidateEmptyPost(t *testing.T) {
 	err := testService.Validate(nil)
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "The post cannot be empty")
+}
+
+func TestValidateEmptyPostTitle(t *testing.T) {
+	post := entity.Post{ID: 1, Title: "", Text: "B"}
+	testService := NewPostService(nil)
+
+	err := testService.Validate(&post)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "The post title cannot be empty", err.Error())
+
 }
